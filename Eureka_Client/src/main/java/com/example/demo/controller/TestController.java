55831dade8c6457c74ed86d69d2fba2d3fccfefe
HZ00M <hzm2373678>
2020-06-06 22:58:48 +0800

    spring security up version

--- Eureka_Client/src/main/java/com/example/demo/controller/TestController.java
+++ Eureka_Client/src/main/java/com/example/demo/controller/TestController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +17,7 @@ public class TestController {
 
     @GetMapping("/info")
     public String info() {
-        @SuppressWarnings("deprecation")
-        ServiceInstance instance = client.getLocalServiceInstance();
-        String info = "host：" + instance.getHost() + "，service_id：" + instance.getServiceId();
-        log.info(info);
-        return info;
+        return "info";
     }
 
     @GetMapping("/hello")
