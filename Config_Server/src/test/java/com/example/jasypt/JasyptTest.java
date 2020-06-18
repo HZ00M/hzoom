e0ba109f6029f89978d65708b8a558a498a63e56
Hzoom <1054743949@qq.com>
2020-06-18 21:06:35 +0800

    change project

--- Config_Server/src/test/java/com/example/jasypt/JasyptTest.java
+++ Config_Server/src/test/java/com/example/jasypt/JasyptTest.java
@@ -1,5 +1,6 @@
 package com.example.jasypt;
 
+import com.example.demo.ConfigServerApplication;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,8 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@WebAppConfiguration
+@SpringBootTest(classes = ConfigServerApplication.class)
 public class JasyptTest {
     @Autowired
     StringEncryptor encryptor;
@@ -21,9 +21,9 @@ public class JasyptTest {
         String url = encryptor.encrypt("https://github.com/HZ00M/config.git");
         String name = encryptor.encrypt("HZ00M");
         String password = encryptor.encrypt("hzm2373678");
-        System.out.println(url+"----------------");
-        System.out.println(name+"----------------");
-        System.out.println(password+"----------------");
+        System.out.println(url + "----------------");
+        System.out.println(name + "----------------");
+        System.out.println(password + "----------------");
         Assert.assertTrue(name.length() > 0);
         Assert.assertTrue(password.length() > 0);
     }
