70f2d658c24c7dccf8a788c013d8b60354063842
Hzoom <1054743949@qq.com>
2020-07-03 18:21:52 +0800

    add xxl-job

---
+++ Eureka_Client/src/main/java/com/example/demo/xxl/XxlJobHandler.java
@@ -0,0 +1,14 @@
+package com.example.demo.xxl;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import com.xxl.job.core.log.XxlJobLogger;
+
+public class XxlJobHandler {
+    @XxlJob(value = "demoJobHandler")
+    public ReturnT<String> execute(String s) throws Exception {
+        XxlJobLogger.log("XXL-JOB, testJobHandler.");
+        System.out.println("XXL-JOB测试");
+        return ReturnT.SUCCESS;
+    }
+}
