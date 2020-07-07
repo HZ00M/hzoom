5043279ba1e99b1fe234f4f9d412657686168694
Hzoom <1054743949@qq.com>
2020-07-07 20:41:33 +0800

    finish sleuth finish xxljob

--- Eureka_Client/src/main/java/com/example/demo/xxl/XxlConfig.java
+++ Eureka_Client/src/main/java/com/example/demo/xxl/XxlConfig.java
@@ -4,9 +4,10 @@ import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
-//@Configuration
+@Configuration
 @Slf4j
 public class XxlConfig {
     @Value("${xxl.job.admin.addresses}")
