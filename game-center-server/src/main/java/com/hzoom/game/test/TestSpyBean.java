b9fac97b4ae26576e340384417e961a031356786
Hzoom <1054743949@qq.com>
2021-02-25 17:32:34 +0800

    ADD multi datasouce starter

--- game-center-server/src/main/java/com/hzoom/game/test/TestSpyBean.java
+++ game-center-server/src/main/java/com/hzoom/game/test/TestSpyBean.java
@@ -1,12 +1,23 @@
 package com.hzoom.game.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TestSpyBean {
+@Slf4j
+public class TestSpyBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware,InitializingBean,BeanPostProcessor, DisposableBean {
     @Autowired
     private TestMockBean testMockBean;
+    private BeanFactory beanFactory;
+    private ApplicationContext applicationContext;
 
     public int getValue() {
         return 3;
@@ -15,6 +26,7 @@ public class TestSpyBean {
     public int getMockBeanLevel() {
         return testMockBean.getValue();
     }
+
     private String getName(int a) {
         return "a";
     }
@@ -49,7 +61,50 @@ public class TestSpyBean {
     }
 
     public void calculate(int a) {
-        int value = (a + 3) *2;
+        int value = (a + 3) * 2;
         this.getName(value);
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        log.info("7 destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("4 afterPropertiesSet  这里可以调用自定义初始化方法");
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        log.info("5 bean初始化前调用postProcessBeforeInitialization {}", beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("6 bean初始化后调用postProcessAfterInitialization {}  bean可以使用了", beanName);
+        return bean;
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        log.info("1 BeanNameAware {}", beanName);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+
+        DefaultListableBeanFactory defaultListableBeanFactory =(DefaultListableBeanFactory) beanFactory;
+        defaultListableBeanFactory.destroySingleton("testSpyBean");
+        log.info("2 setBeanFactory");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        log.info("3 setApplicationContext");
     }
 }
